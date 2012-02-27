59c50e4579a383796d6e35b9177231c8842841f0
mhausenblas <michael.hausenblas@deri.org>
2012-02-27 09:51:12 +0000

    swtiched over to using BeautifulSoup for link extraction

--- racoon.py
+++ racoon.py
@@ -16,7 +16,7 @@ import getopt
 import StringIO
 from urllib2 import Request, urlopen, URLError, HTTPError
 from urlparse import urljoin, urlparse
-from HTMLParser import HTMLParser
+from BeautifulSoup import BeautifulSoup, SoupStrainer
 from pprint import pprint
 import simplejson as json
 import time
@@ -29,38 +29,6 @@ DEFAULT_POLITNESS = 0.2 # that is, wait 0.2 sec between two sub-sequent requests
 HTML_CONTENT_TYPES = ['text/html', 'application/xhtml+xml'] # based on http://www.whatwg.org/specs/web-apps/current-work/multipage/iana.html
 EXCLUDED_URI_REFS = ['mailto', 'ftp']
 
-class LinkExtractor(HTMLParser):
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.links = []
-		self.link_text = {}
-		self.is_link = False
-		self.current_link = ''
-	
-	def handle_starttag(self, tag, attrs):
-		if tag != 'a':
-			return
-		self.is_link = True
-		for name, value in attrs:
-			if name == 'href':
-				self.links.append(value)
-				self.current_link = value
-				break
-			else:
-				return
-
-	def handle_data(self, data):
-		if self.is_link:
-			#if DEBUG: logging.debug("link text: %s" %data)
-			self.link_text[self.current_link] = data
-
-	def handle_endtag(self, tag):
-		self.is_link = False
-			
-	def get_links(self):
-		return self.links
-	
-	
 class RCrawler(object):
 	# init with the base URL of the site and a default politness of 1s wait between two hits
 	def __init__(self, base, politeness = 1):
@@ -71,9 +39,9 @@ class RCrawler(object):
 		self.rp = robotparser.RobotFileParser()
 		self.rp.set_url(urljoin(self.base, 'robots.txt'))
 		self.rp.read()
-		
+	
 	# crawl from a seed URL
-	def crawl(self, host_loc, cur_loc):
+	def crawl(self, host_loc, cur_loc, link_text=''):
 		# check if we're allowed to crawl it (as of robots.txt) and we're not going in circles and ignore schemes such as mailto: and ftp:
 		if self.rp.can_fetch("*", cur_loc) and cur_loc not in self.seen and not urlparse(cur_loc).scheme in EXCLUDED_URI_REFS: 
 			self.seen.append(cur_loc) # remeber what links we have already traversed
@@ -86,32 +54,34 @@ class RCrawler(object):
 					self.follow_hyperlinks(cur_loc, content)
 				else:
 					if DEBUG: logging.debug('host: %s, doc: %s' %(host_loc, cur_loc))
-					# TODO: add anchor text, title, etc.
+					if link_text: lt = link_text
+					else: lt = ''
 					try:
-						self.nuggets[host_loc].append({ 'URL' : cur_loc, 'type' : headers['content-type'] , 'size' : headers['content-length']})
+						self.nuggets[host_loc].append({ 'URL' : cur_loc, 'text' : lt, 'type' : headers['content-type'] , 'size' : headers['content-length']})
 					except KeyError, e:
 						self.nuggets[host_loc] = []
-						self.nuggets[host_loc].append({ 'URL' : cur_loc, 'type' : headers['content-type'] , 'size' : headers['content-length']})
+						self.nuggets[host_loc].append({ 'URL' : cur_loc, 'text' : lt, 'type' : headers['content-type'] , 'size' : headers['content-length']})
 	
+	# check if we're really dealing with an HTML media type
 	def is_html(self, content_type):
 		if content_type in HTML_CONTENT_TYPES: return True
 		else: return False
 	
-	
 	# extract all hyperlinks (<a href='' ...>) from an HTML content and follow them recursively
 	def follow_hyperlinks(self, host_loc, content):
-		le = LinkExtractor()
-		le.feed(content) # extract links
-		links = le.get_links()
-		if DEBUG: logging.debug('outgoing: %s' %links)
-		for link in links:
-			if DEBUG: logging.debug('checking %s' %link)
-			if not link.startswith('http://'): # stay in the domain, only relative links, no outbound links 
-				logging.info('checking: %s' %urljoin(host_loc, link))
-				self.crawl(host_loc, urljoin(host_loc, link))
-			else:
-				logging.info('ignoring: %s' %link)
-
+		links = []
+		links_text = {}
+		for link in BeautifulSoup(content, parseOnlyThese=SoupStrainer('a')):
+			if link.has_key('href'):
+				links.append(link['href'])
+				links_text[link['href']] = link.renderContents()
+				if DEBUG: logging.debug('checking %s' %link)
+				if not link['href'].startswith('http://'): # stay in the domain, only relative links, no outbound links 
+					logging.info('checking: %s' %urljoin(host_loc, link['href']))
+					self.crawl(host_loc, urljoin(host_loc, link['href']), links_text[link['href']])
+				else:
+					logging.info('ignoring: %s' %link['href'])
+	
 	# dereference a URL and try to return the HTTP headers and the content 
 	def get(self, url):
 		try:
@@ -132,7 +102,6 @@ class RCrawler(object):
 			headers = response.info()
 			return (headers, content)
 	
-
 	# return the description of the crawled Open Data documents
 	def desc(self, as = 'text'):
 		if as == 'text':
@@ -140,6 +109,7 @@ class RCrawler(object):
 		if as == 'json':
 			encoder = json.JSONEncoder()
 			return encoder.encode(self.nuggets)
+	
 	
 def usage():
 	print("Usage: python racoon.py -p|-j {seed URL} where -p produces a plain output and -j a JSON output.")
@@ -157,6 +127,7 @@ if __name__ == "__main__":
 				seed_url = arg
 				r = RCrawler(seed_url, DEFAULT_POLITNESS)
 				r.crawl(seed_url, seed_url)
+				print("result:")
 				pprint(r.desc())
 			elif opt in ("-j", "--json"):
 				print("starting crawl with JSON output at [%s] ..." %arg)
