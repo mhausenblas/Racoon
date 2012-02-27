8be636c808873182f868ccee7211a62676167854
mhausenblas <michael.hausenblas@deri.org>
2012-02-27 12:30:46 +0000

    fixes for limit

--- racoon.py
+++ racoon.py
@@ -25,8 +25,8 @@ import robotparser
 # Racoon config
 logging.basicConfig(level=logging.DEBUG)
 DEBUG = False
-DEFAULT_POLITNESS = 0.2 # that is, wait 0.2 sec between two sub-sequent requests at a site
-LIMIT = 10 # visit only LIMIT pages
+DEFAULT_POLITNESS = 0.1 # that is, wait 0.2 sec between two sub-sequent requests at a site
+LIMIT = -1 # visit only LIMIT pages - if you want to crawl without limits, set to -1
 HTML_CONTENT_TYPES = ['text/html', 'application/xhtml+xml'] # based on http://www.whatwg.org/specs/web-apps/current-work/multipage/iana.html
 EXCLUDED_URI_REFS = ['mailto', 'ftp']
 
@@ -42,29 +42,44 @@ class RCrawler(object):
 		self.rp.read()
 	
 	# crawl from a seed URL
-	def crawl(self, host_loc, cur_loc, link_text=''):
-		if DEBUG: logging.debug('link count: %s' %len(self.seen))
-		if len(self.seen) < LIMIT:
-			# check if we're allowed to crawl it (as of robots.txt) and we're not going in circles and ignore schemes such as mailto: and ftp:
-			if self.rp.can_fetch("*", cur_loc) and cur_loc not in self.seen and not urlparse(cur_loc).scheme in EXCLUDED_URI_REFS: 
-				self.seen.append(cur_loc) # remeber what links we have already traversed
-				if DEBUG: logging.debug('visited: %s' %self.seen)
-				(headers, content) = self.get(cur_loc)
-				if headers:
-					if DEBUG: logging.debug('url: %s, type: %s, size: %s' %(cur_loc, headers['content-type'], headers['content-length']))
-					if(self.is_html(headers['content-type'])):
-						time.sleep(self.politeness)
-						self.follow_hyperlinks(cur_loc, content)
-					else:
-						if DEBUG: logging.debug('host: %s, doc: %s' %(host_loc, cur_loc))
-						if link_text: lt = link_text
-						else: lt = ''
-						try:
-							self.nuggets[host_loc].append({ 'URL' : cur_loc, 'text' : lt, 'type' : headers['content-type'] , 'size' : headers['content-length']})
-						except KeyError, e:
-							self.nuggets[host_loc] = []
-							self.nuggets[host_loc].append({ 'URL' : cur_loc, 'text' : lt, 'type' : headers['content-type'] , 'size' : headers['content-length']})
-		else: return
+	def crawl(self, host_loc, cur_loc, link_text='', limit = LIMIT):
+		if limit > 0:
+			if len(self.seen) <= LIMIT:
+				if DEBUG: logging.debug('link count: %s' %len(self.seen))
+				self.fetch_extract(host_loc, cur_loc, link_text, limit)
+			else: return
+		else:
+			self.fetch_extract(host_loc, cur_loc, link_text, limit)
+	
+	# fetches from location, extracts hyperlinks and recursively crawls them
+	def fetch_extract(self, host_loc, cur_loc, link_text='', limit = LIMIT):
+		# check if we're allowed to crawl it (as of robots.txt) and we're not going in circles and ignore schemes such as mailto: and ftp:
+		if self.rp.can_fetch("*", cur_loc) and cur_loc not in self.seen and not urlparse(cur_loc).scheme in EXCLUDED_URI_REFS: 
+			self.seen.append(cur_loc) # remeber what links we have already traversed
+			if DEBUG: logging.debug('visited: %s' %self.seen)
+			(headers, content) = self.get(cur_loc)
+			if headers:
+				try:
+					ct = headers['content-type']
+				except:
+					ct = 'unknown'
+				try:
+					cl = headers['content-length']
+				except:
+					cl = 'unknown'
+				logging.info('currently looking at url: %s, type: %s, size: %s' %(cur_loc, ct, cl))
+				if(self.is_html(ct)): # another HTML page, recursively crawl
+					time.sleep(self.politeness)
+					self.follow_hyperlinks(cur_loc, content, limit)
+				else: # potentially found a nugget, add to list
+					if link_text: lt = link_text
+					else: lt = ''
+					logging.info('adding nugget: %s from page: %s' %(cur_loc, host_loc))
+					try:
+						self.nuggets[host_loc].append({ 'URL' : cur_loc, 'text' : lt, 'type' : ct , 'size' : cl})
+					except KeyError, e:
+						self.nuggets[host_loc] = []
+						self.nuggets[host_loc].append({ 'URL' : cur_loc, 'text' : lt, 'type' : ct , 'size' : cl})
 	
 	# check if we're really dealing with an HTML media type
 	def is_html(self, content_type):
@@ -72,7 +87,7 @@ class RCrawler(object):
 		else: return False
 	
 	# extract all hyperlinks (<a href='' ...>) from an HTML content and follow them recursively
-	def follow_hyperlinks(self, host_loc, content):
+	def follow_hyperlinks(self, host_loc, content, limit):
 		links = []
 		links_text = {}
 		for link in BeautifulSoup(content, parseOnlyThese=SoupStrainer('a')):
@@ -81,10 +96,10 @@ class RCrawler(object):
 				links_text[link['href']] = link.renderContents()
 				if DEBUG: logging.debug('checking %s' %link)
 				if not link['href'].startswith('http://'): # stay in the domain, only relative links, no outbound links 
-					logging.info('checking: %s' %urljoin(host_loc, link['href']))
-					self.crawl(host_loc, urljoin(host_loc, link['href']), links_text[link['href']])
+					logging.info('consider outgoing: %s' %urljoin(host_loc, link['href']))
+					self.crawl(host_loc, urljoin(host_loc, link['href']), links_text[link['href']], limit)
 				else:
-					logging.info('ignoring: %s' %link['href'])
+					logging.info('ignoring outgoing: %s' %link['href'])
 	
 	# dereference a URL and try to return the HTTP headers and the content 
 	def get(self, url):
@@ -142,3 +157,5 @@ if __name__ == "__main__":
 		print str(err)
 		usage()
 		sys.exit(2)
+	
+	
